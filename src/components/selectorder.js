c7ebd0b81d7a259825f53fc0a6db6006e9052cee
nftcrias <nftcrias@gmail.com>
2023-05-02 14:38:46 -0300

    cwc

--- src/components/selectorder.js
+++ src/components/selectorder.js
@@ -7,8 +7,9 @@ const SelectOrder = ({wallets}) => {
 
     // Fired when the order as been choseen
     // selected equals to 1 = Angel / 2 = Human / 3 = Demon
-    function SelectedOrderCallBack(selected) {
+    function SelectedOrder(selected) {
         // after select save in db and redirect to /collect
+        
     }
 
     return (
@@ -17,15 +18,15 @@ const SelectOrder = ({wallets}) => {
             <div class="selectionContainer">
                 <div class="selectItem">
                     <p>Angel</p>
-                    <img class="selectImage" src={Angel} alt="" onClick={SelectedOrderCallBack.bind(this, 1)} />
+                    <img class="selectImage" src={Angel} alt="" onClick={SelectedOrder.bind(this, 1)} />
                 </div>
                 <div class="selectItem">
                     <p>Human</p>
-                    <img class="selectImage" src={Human} alt="" onClick={SelectedOrderCallBack.bind(this, 2)} />
+                    <img class="selectImage" src={Human} alt="" onClick={SelectedOrder.bind(this, 2)} />
                 </div>
                 <div class="selectItem">
                     <p>Demon</p>
-                    <img class="selectImage" src={Demon} alt="" onClick={SelectedOrderCallBack.bind(this, 3)} />
+                    <img class="selectImage" src={Demon} alt="" onClick={SelectedOrder.bind(this, 3)} />
                 </div>
             </div>
         </div>
